0ebb1450c18771ea56282d4a27a4066a95934046
ashutosh_k_akhouri <ashutosh.akhouri@gmail.com>
2020-08-06 17:23:00 +0530

    Passport local strategy

---
+++ backend/config/passport.js
@@ -0,0 +1,28 @@
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var User = require('../models/user');
+
+// Passport is a Node module that simplifies the process of handling authentication in Express.
+// It provides a common gateway to work with many different authentication “strategies”,
+// such as logging in with Facebook, Twitter or Oauth.
+// The strategy we’ll use is called “local”, as it uses a username and password stored locally.
+
+// Read more here: http://www.passportjs.org/docs/authenticate/
+
+passport.use(new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'password'
+},
+    function (username, password, done) {
+        User.findOne({ email: username }, function (err, user) {
+            if (err) { return done(err); }
+            if (!user) {
+                return done(null, false, { message: 'Incorrect email.' });
+            }
+            if (!user.validPassword(password)) {
+                return done(null, false, { message: 'Incorrect password.' });
+            }
+            return done(null, user);
+        });
+    }
+));
